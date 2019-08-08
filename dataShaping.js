a046a15a8b4a92bfcefcd8cbe4ed2f8201dbe88f
Andrew Morris <apmorris83@gmail.com>
2019-08-08 20:31:29 +0100

    Get data in correct format

--- dataShaping.js
+++ dataShaping.js
@@ -1,19 +1,48 @@
-const data = [
+const data = [ // app data structure
     {section:'food',row:'groceries',amount:13.42,note:'Aldi',date:'2019-08-05T18:38:17.792Z'},
     {section:'food',row:'groceries',amount:26.7,note:'B&M',date:'2019-08-05T18:38:17.792Z'},
-    {section:'food',row:'restaurants',amount:26.7,note:'Lowes Arms',date:'2019-08-05T18:38:17.792Z'},
+    {section:'food',row:'restaurants',amount:26.7,note:'Lowes Arms',date:'2019-01-05T18:38:17.792Z'},
     {section:'personal',row:'squash',amount:1.5,note:'Court',date:'2019-07-05T18:38:17.792Z'},
     {section:'personal',row:'squash',amount:2,note:'Light money',date:'2019-07-05T18:38:17.792Z'},
     {section:'blackRio',row:'petrol',amount:35.01,note:'Asda',date:'2019-07-05T18:38:17.792Z'},
 ];
-const getUniqueBy = (arr,val) => {
+const getUniqueBy = (arr,val) => { // helper function
     return arr.reduce((acc,cur) => {
         if(acc.indexOf(cur[val]) < 0) acc.push(cur[val]);
         return acc;
     },[]);
 }
+const arrangeByKey = (arr,key) => { // helper function
+    return arr.reduce((acc,cur) => {
+        acc[cur[key]] = acc[cur[key]] || [];
+        acc[cur[key]].push(cur);
+        return acc;
+    }, {});
+}
+const roundValue = (val) => val.toFixed(2); // helper function
+const buildMonthsArrayFromASingleRow = (arr) => { // helper function
+    const dataToUse = arr.map(ent => {
+        return {...ent,month:new Date(ent.date).getMonth()}
+    })
+    const res = arrangeByKey(dataToUse,'month');
+    const rows = [0,0,0,0,0,0,0,0,0,0,0,0];
+    for(let i=0; i<rows.length-1; i++){
+        if(res[i]) rows[i] = res[i];
+    }
+    const total = getRowTotal(rows);
+    return rows.concat([Number(roundValue(total)),Number(roundValue(total/12))]);
+}
+const getSectionTotals = (rows) => { // helper function
+    return rows.map(row => {
+        return row.data.map(data => {
+            if(typeof data === 'number') return data;
+            return data.reduce((acc,cur) => acc + cur.amount,0)
+        })
+    }).reduce((r,a) => r.map((b,i) => a[i] + b)) // this adds all the arrays together
+}
+const getRowTotal = (row) => row.reduce((acc,cur) => Array.isArray(cur) ? acc + cur.reduce((ac,cu) => ac + cu.amount,0) : acc + cur,0); // helper function
 const getSections = () => getUniqueBy(data,'section'); // get sections for the history filters and the toolbar
-const getRowsBySection = (section) => getUniqueBy(data.filter(ent => ent.section === section),'row'); // get rows for when selecting a row
+const getRowsBySection = (section) => getUniqueBy(data.filter(ent => ent.section === section),'row'); // get rows by section for the history filters and the toolbar
 const filterData = (filters) => { // filter data by section, row, month and year
     return data.filter(ent => {
         const bySection = filters.section === 'all' ? true : ent.section === filters.section;
@@ -23,48 +52,32 @@ const filterData = (filters) => { // filter data by section, row, month and year
         return bySection && byRow && byMonth && byYear;
     })
 }
-const buildSection = () => { // build an array of sections, which has a rows array including all the associated rows
-    return getSections().map(section => {
+const buildDataForRendering = () => { // build an array of sections, which has a rows array including all the associated rows
+    const dataArrangedBySection = arrangeByKey(data,'section');
+    const dataAndRowsOrganised = Object.values(dataArrangedBySection).map(row => {
         return {
-            name:section,
-            rows:data.map(ent => getRowsBySection(section).includes(ent.row) ? {name:ent.row, amount:ent.amount, note:ent.note, date:ent.date } : null).filter(e=>e)
-        };
+            name:row[0].section,
+            rows:arrangeByKey(row,'row')
+        }
+    });
+    const rowsWithDataArrays = dataAndRowsOrganised.map(section => {
+        return {
+            ...section,
+            rows:Object.values(section.rows).map(row => {
+                return {
+                    name:row[0].row,
+                    data:buildMonthsArrayFromASingleRow(row)
+                }
+            })
+        }
+    });
+    return rowsWithDataArrays.map(section => {
+        const dataArray = getSectionTotals(section.rows);
+        return {
+            ...section,
+            data:dataArray
+        }
     })
+    
 }
-
-/*
-console.log(['all',...getSections()]); 
-// [ 'all', 'food', 'personal', 'blackRio' ]
-
-console.log(['all',...getRowsBySection('food')]);
-// [ 'all', 'groceries', 'restaurants' ]
-
-console.log(filterData({section:'food',row:'restaurants',month:7,year:2019})); 
-// [ { section: 'food', row: 'restaurants', ...} ]
-*/
-console.log(buildSection())
-/* returns the below array
-[ 
-    { 
-        name: 'food',
-        rows: [ 
-            { name: 'groceries', amount: 13.42, note: 'Aldi', date: '2019-08-05T18:38:17.792Z' },
-            { name: 'groceries', amount: 26.7, note: 'B&M', date: '2019-08-05T18:38:17.792Z' },
-            { name: 'restaurants', amount: 26.7, note: 'Lowes Arms', date: '2019-08-05T18:38:17.792Z' } 
-        ] 
-    },
-    { 
-        name: 'personal',
-        rows: [ 
-            { name: 'squash', amount: 1.5, note: 'Court', date: '2019-07-05T18:38:17.792Z' },
-            { name: 'squash', amount: 2, note: 'Light money', date: '2019-07-05T18:38:17.792Z' } 
-        ] 
-    },
-    { 
-        name: 'blackRio',
-        rows: [ 
-            { name: 'petrol', amount: 35.01, note: 'Asda', date: '2019-07-05T18:38:17.792Z' } 
-        ] 
-    }
-]
- */
+console.log(JSON.stringify(buildDataForRendering()))
